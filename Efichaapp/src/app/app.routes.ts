d0d2ebbac0a959f8d2d3f17461e03861e0d3679d
Maria Eduarda <vantev806@gmail.com>
2025-11-14 13:57:00 -0300

    feat: Criando home, home-admin e outros

--- Efichaapp/src/app/app.routes.ts
+++ Efichaapp/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 
@@ -7,8 +8,30 @@ export const routes: Routes = [
         loadComponent: () => import('./component/login/login.component').then((m) => m.LoginComponent),
     },
 
-    // {
-    //     path: 'paciente',
-    //     loadComponent
-    // }
+    {
+        path: 'paciente',
+    loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component').then((m) => m.RestritoLayoutComponent),
+        canActivate: [AuthGuard],
+        data: { role: 'PACIENTE' },
+
+        children: [
+            {
+                path: 'inicio',
+                loadComponent: () => import('./component/paciente/home/home.component').then(m => m.HomeComponent),
+            },
+        ]
+    }
+,
+    {
+        path: 'admin',
+        loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component').then((m) => m.RestritoLayoutComponent),
+        canActivate: [AuthGuard],
+        data: { role: 'ADMIN' },
+        children: [
+            {
+                path: 'inicio',
+                loadComponent: () => import('./admin/home-admin/home-admin.component').then(m => m.HomeAdminComponent),
+            },
+        ]
+    }
 ];
