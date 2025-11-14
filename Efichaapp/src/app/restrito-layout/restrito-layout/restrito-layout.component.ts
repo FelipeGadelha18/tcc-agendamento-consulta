d02c208e307289308d27ec673637818e978b08fe
Maria Eduarda <vantev806@gmail.com>
2025-11-14 13:55:36 -0300

    feat: Criando o layout do Sistema

---
+++ Efichaapp/src/app/restrito-layout/restrito-layout/restrito-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component,HostListener } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../component/layouts/footer/footer.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-restrito-layout',
+  imports: [FooterComponent, RouterOutlet,],
+  templateUrl: './restrito-layout.component.html',
+  styleUrls: ['./restrito-layout.component.scss']
+})
+export class RestritoLayoutComponent {
+      
+}
