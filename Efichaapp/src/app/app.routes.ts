7f871d8892f08c9a76c8603b9326814325e7273a
Felipe <felipegadelha062@gmail.com>
2025-11-17 19:18:14 -0300

    Fix - Mudando Rota

--- Efichaapp/src/app/app.routes.ts
+++ Efichaapp/src/app/app.routes.ts
@@ -3,39 +3,51 @@ import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 
+    // 👉 Redireciona automaticamente para /login ao iniciar a aplicação
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+
     {
         path: 'login',
-        loadComponent: () => import('./component/login/login.component').then((m) => m.LoginComponent),
+        loadComponent: () => import('./component/login/login.component')
+            .then((m) => m.LoginComponent),
     },
 
     {
         path: 'cadastro',
-        loadComponent: () => import('./component/cadastro/cadastro.component').then((m) => m.CadastroComponent),
+        loadComponent: () => import('./component/cadastro/cadastro.component')
+            .then((m) => m.CadastroComponent),
     },
 
     {
         path: 'paciente',
-    loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component').then((m) => m.RestritoLayoutComponent),
+        loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component')
+            .then((m) => m.RestritoLayoutComponent),
         canActivate: [AuthGuard],
         data: { role: 'PACIENTE' },
-
         children: [
             {
                 path: 'inicio',
-                loadComponent: () => import('./component/paciente/home/home.component').then(m => m.HomeComponent),
+                loadComponent: () => import('./component/paciente/home/home.component')
+                    .then(m => m.HomeComponent),
             },
         ]
-    }
-,
+    },
+
     {
         path: 'admin',
-        loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component').then((m) => m.RestritoLayoutComponent),
+        loadComponent: () => import('./restrito-layout/restrito-layout/restrito-layout.component')
+            .then((m) => m.RestritoLayoutComponent),
         canActivate: [AuthGuard],
         data: { role: 'ADMIN' },
         children: [
             {
                 path: 'inicio',
-                loadComponent: () => import('./admin/home-admin/home-admin.component').then(m => m.HomeAdminComponent),
+                loadComponent: () => import('./admin/home-admin/home-admin.component')
+                    .then(m => m.HomeAdminComponent),
             },
         ]
     }
