fac0a234bca98ba898acd9602a53bb135941dc57
Maria Eduarda <vantev806@gmail.com>
2025-11-24 13:53:49 -0300

    feat: Implementando card com navegação

--- Efichaapp/src/app/component/layouts/footer/footer.component.ts
+++ Efichaapp/src/app/component/layouts/footer/footer.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
--- Efichaapp/src/app/component/paciente/home/home.component.ts
+++ Efichaapp/src/app/component/paciente/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgForOf, NgClass } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
+  standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [NgForOf, RouterLink, NgClass, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  cards = [
+    { icon: 'pi pi-map-marker', title: 'Postos<br>Próximos', route: '/postos-proximos' },
+    { icon: 'pi pi-calendar', title: 'Reservar Ficha', route: '/reservar' },
+    { icon: 'pi pi-file', title: 'Minhas Fichas', route: '/fichas' },
+    { icon: 'pi pi-user', title: 'Perfil', route: '/perfil' },
+  ];
 
 }
