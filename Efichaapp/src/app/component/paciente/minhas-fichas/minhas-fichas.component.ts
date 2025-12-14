a5b220ebf8efd33d7cb3d4f650dd2a540095b46b
Felipe Gadelha <felipegadelha062@gmail.com>
2025-12-14 20:48:21 -0300

    Cancelar

--- Efichaapp/src/app/component/paciente/minhas-fichas/minhas-fichas.component.ts
+++ Efichaapp/src/app/component/paciente/minhas-fichas/minhas-fichas.component.ts
@@ -28,8 +28,30 @@ export class MinhasFichasComponent implements OnInit {
     this.http
       .get<any[]>(`http://localhost:8080/reservas/por-paciente/${this.pacienteLogado.id}`)
       .subscribe({
-        next: (dados) => this.reservas = dados,
+        next: (dados) => {
+          // Garante status
+          this.reservas = dados.map(r => ({
+            ...r,
+            status: r.status || 'CONFIRMADA'
+          }));
+        },
         error: () => alert('Erro ao carregar suas fichas')
+      });
+  }
+
+  cancelarFicha(idReserva: number) {
+    if (!confirm('Deseja realmente cancelar esta ficha?')) {
+      return;
+    }
+
+    this.http
+      .put(`http://localhost:8080/reservas/cancelar/${idReserva}`, {})
+      .subscribe({
+        next: () => {
+          alert('Ficha cancelada com sucesso!');
+          this.carregarMinhasReservas();
+        },
+        error: () => alert('Erro ao cancelar ficha')
       });
   }
 }
