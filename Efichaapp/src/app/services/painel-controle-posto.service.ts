a4a89c00868739cf6175c16991f5a2876e27f219
Felipe Gadelha <felipegadelha062@gmail.com>
2026-04-21 15:02:02 -0300

    feat: Ajustar formatação e espaçamento em componentes HTML e SCSS

--- Efichaapp/src/app/services/painel-controle-posto.service.ts
+++ Efichaapp/src/app/services/painel-controle-posto.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface Posto {
-  id:number;
+  id: number;
   nome: string;
   endereco: string;
   capacidadeDiaria: number;
