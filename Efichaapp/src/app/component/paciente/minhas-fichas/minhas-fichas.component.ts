f9b68dfb33911ab18ddbcbce793f29e42f2758a5
Maria Eduarda <vantev806@gmail.com>
2026-08-12 21:56:24 -0300

    feat: Refatoração do layout e dos estilos dos componentes do paciente
    -Implementada uma barra de navegação responsiva em vários componentes do paciente (minhas-fichas, perfil, reservar-ficha, postos-proximos), com links para as principais seções.
    
    -Melhoradas as seções de cabeçalho, com estilos aprimorados e botões de navegação para voltar.
    
    -Atualizados os estilos SCSS para melhorar a consistência visual e a responsividade, incluindo novas variáveis para cores e gradientes.
    Adicionada funcionalidade de menu mobile para melhorar a usabilidade em telas menores.
    
    -Ajustados os templates dos componentes para refletir a nova estrutura e os novos estilos.
    
    -Incluídos itens de menu para navegação nos componentes perfil e reservar-ficha.

--- Efichaapp/src/app/component/paciente/minhas-fichas/minhas-fichas.component.ts
+++ Efichaapp/src/app/component/paciente/minhas-fichas/minhas-fichas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
@@ -12,7 +13,7 @@ import { ReservaService } from '../../../services/reservar.service';
 @Component({
   selector: 'app-minhas-fichas',
   standalone: true,
-  imports: [CommonModule, ButtonModule, MenuModule],
+  imports: [CommonModule, ButtonModule, MenuModule, RouterLink],
   templateUrl: './minhas-fichas.component.html',
   styleUrls: ['./minhas-fichas.component.scss']
 })
